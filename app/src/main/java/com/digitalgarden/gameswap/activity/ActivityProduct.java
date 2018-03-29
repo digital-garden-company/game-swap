6b329d5a47d9556a373ada329031082b306a6ac8
greatergoodguy <xuetomyu@gmail.com>
2018-03-28 22:00:30 -0500

    Added menu for the product screen

--- app/src/main/java/com/digitalgarden/gameswap/activity/ActivityProduct.java
+++ app/src/main/java/com/digitalgarden/gameswap/activity/ActivityProduct.java
@@ -1,6 +1,9 @@
 package com.digitalgarden.gameswap.activity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.digitalgarden.gameswap.R;
 
@@ -10,5 +13,29 @@ public class ActivityProduct extends Activity_Base {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_product, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_product_share:
+                return true;
+            case R.id.menu_product_save:
+                return true;
+            case R.id.menu_product_edit:
+                return true;
+            case R.id.menu_product_delete:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
