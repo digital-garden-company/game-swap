2124de3f9ce40819a518569051dac2cc4710ad52
greatergoodguy <xuetomyu@gmail.com>
2018-06-10 22:50:18 -0500

    Added ability to upload image to firebase storage

--- app/src/main/java/com/digitalgarden/gameswap/activity/ActivityAbout.java
+++ app/src/main/java/com/digitalgarden/gameswap/activity/ActivityAbout.java
@@ -3,10 +3,13 @@ package com.digitalgarden.gameswap.activity;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
@@ -14,15 +17,26 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.digitalgarden.gameswap.R;
+import com.digitalgarden.gameswap.toolbox.Toolbox;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 public class ActivityAbout extends Activity_Base {
 
     Uri fileUri;
     ImageView imageView;
+
+    // Get a non-default Storage bucket
+    FirebaseStorage storage = FirebaseStorage.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,24 +46,30 @@ public class ActivityAbout extends Activity_Base {
         imageView = (ImageView) findViewById(R.id.layout_image);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            findViewById(R.id.layout_button).setEnabled(false);
+            findViewById(R.id.layout_button_takepicture).setEnabled(false);
             ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
         }
 
-        findViewById(R.id.layout_button).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.layout_button_upload).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
+        findViewById(R.id.layout_button_takepicture).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 takePicture();
             }
         });
-
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == 0) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                findViewById(R.id.layout_button).setEnabled(true);
+                findViewById(R.id.layout_button_takepicture).setEnabled(true);
             }
         }
     }
@@ -83,5 +103,36 @@ public class ActivityAbout extends Activity_Base {
 
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         return new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
+    }
+
+
+
+    private void uploadImage() {
+        StorageReference storageRef = storage.getReference();
+        //StorageReference imageRef = storageRef.child("images/picture.jpg");
+        StorageReference imageRef = storageRef.child("images/" + UUID.randomUUID() + ".jpg");
+
+        // Get the data from an ImageView as bytes
+        imageView.setDrawingCacheEnabled(true);
+        imageView.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = imageRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+                Toolbox.log(TAG, "onFailure()");
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                Toolbox.log(TAG, "onSuccess()");
+            }
+        });
     }
 }
